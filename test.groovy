fb2b4a289b9a3b7951754d2c33bef2725daa8d91
Dov_Dembin <Dov.Dembin@Dell.com>
2023-02-13 23:42:26 +0200

    d

--- test.groovy
+++ test.groovy
@@ -8,10 +8,5 @@ def countTestOTel(duration, end_status) {
 try {
  countTestOTel(duration, end_status)
 } catch (Exception e) {
-        println("ERROR: UNEXPECTED Jenkins FAILURE, marking build as FAILED")
-        println(e.toString())
-        currentBuild.result = 'FAILED'
-        withEnv(env_list) {
-            updateGitHubAndDBAfterUnexpectedFailure()
-        }
+         
     }
