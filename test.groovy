b1f45de26b3c4e4cfea0dd58e7a9a1739ded2a7a
Dov_Dembin <Dov.Dembin@Dell.com>
2023-02-14 19:46:20 +0200

    ff

--- test.groovy
+++ test.groovy
@@ -1,14 +1,8 @@
-@Grapes([
-   @Grab(group='io.opentelemetry', module='opentelemetry-bom', version='1.20.1', type='pom'),
-   @Grab(group='io.opentelemetry', module='opentelemetry-api', version='1.23.0'),
-   @Grab(group='io.opentelemetry', module='opentelemetry-sdk', version='1.23.0'),
-   @Grab(group='io.opentelemetry', module='opentelemetry-exporter-otlp', version='1.22.0'),
-   @Grab(group='io.opentelemetry', module='opentelemetry-semconv', version='1.20.1-alpha', scope='runtime')
-])
+
 
 
 def countTestOTel(duration, end_status) {
-     
+     ExampleConfiguration exampleConfiguration = new ExampleConfiguration()
 }
 
 try {
