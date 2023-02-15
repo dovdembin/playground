2c1667a37a2ce5f1bf4cce11c1692fa798730701
Dov_Dembin <Dov.Dembin@Dell.com>
2023-02-15 12:43:35 +0200

    dfd

--- test.groovy
+++ test.groovy
@@ -1,12 +1,7 @@
 def countTestOTel() {
-    openTelemetry = ExampleConfiguration.initOpenTelemetry("http://localhost:4317");
-    tracer = openTelemetry.getTracer("io.opentelemetry.example");
-    exampleSpan = tracer.spanBuilder("oteljenkins").startSpan();
-    scope = exampleSpan.makeCurrent() 
-    exampleSpan.setAttribute("good", "true");
-    exampleSpan.setAttribute("exampleNumber", i);
-    Thread.sleep(100);    
-    exampleSpan.end();   
+    GroovyShell shell = new GroovyShell()
+def tools = shell.parse(new File('function_tools.gvy'))
+tools.greet()  
 }
 
 try {
