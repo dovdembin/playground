1f5447cf52142fbe9bd04f60ec50645ed32223c3
Dov_Dembin <Dov.Dembin@Dell.com>
2023-02-15 14:44:00 +0200

    ff

--- test.groovy
+++ test.groovy
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('Example') { 
             steps {
-                global("koko", "Thursday") 
+                global(name:"koko", dayOfWeek:"Thursday") 
             }
         }
     }
