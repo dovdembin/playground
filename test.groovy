19c6b9e3bb27f404525933ecf23a77ae4f0efab5
Dov_Dembin <Dov.Dembin@Dell.com>
2023-02-16 14:33:47 +0200

    dd

--- test.groovy
+++ test.groovy
@@ -3,7 +3,7 @@
  
  try {
     stage("Running Stage: ") {
-        log.func(endpoint=env.OTEL_EXPORTER_OTLP_ENDPOINT)
+        log.func(endpoint:env.OTEL_EXPORTER_OTLP_ENDPOINT)
     }
 } catch(Exception e) {
 
