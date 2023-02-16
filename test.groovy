14ff48ad16589a4e42768980a91f1293fcea6f4e
Dov_Dembin <Dov.Dembin@Dell.com>
2023-02-16 14:54:13 +0200

    ss

--- test.groovy
+++ test.groovy
@@ -1,13 +1,10 @@
 @Library("shared-library") _
  
  
- try {
-    stage("Running Stage: ") {
-        log.otelcli(
-            endpoint:env.OTEL_EXPORTER_OTLP_ENDPOINT,
-            service_name:"otel-cli-java"
-            )
-    }
-} catch(Exception e) {
 
+stage("Running Stage: ") {
+    log.otelcli(
+        endpoint:env.OTEL_EXPORTER_OTLP_ENDPOINT,
+        service_name:"otel-cli-java"
+        )
 }
