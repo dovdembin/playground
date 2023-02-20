c74f27a16d78bd58493aad69f3ae801ef418d05b
Dov_Dembin <Dov.Dembin@Dell.com>
2023-02-21 00:47:19 +0200

    ddd

--- vars/log.groovy
+++ vars/log.groovy
@@ -25,7 +25,7 @@ import io.opentelemetry.sdk.resources.Resource;
 import io.opentelemetry.sdk.trace.SdkTracerProvider;
 import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
 import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
-import io.grpc.*
+
 
 public class Otel {
 
