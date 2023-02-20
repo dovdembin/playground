74cd0f53d4316faa9a0e1d873fce25cd97796395
Dov_Dembin <Dov.Dembin@Dell.com>
2023-02-20 10:05:20 +0200

    fghj

--- test.groovy
+++ test.groovy
@@ -3,8 +3,23 @@
  
 
 stage("Running Stage: ") {
-    log.otelcli(
-        endpoint:env.OTEL_EXPORTER_OTLP_ENDPOINT,
-        servicename:"otel-cli-java3"
-        )
+    Otel otel = new Otel(endpoint:endpoint:env.OTEL_EXPORTER_OTLP_ENDPOINT, counter:"tridevlab.test-counter").meterCounter(
+			koko:"loko", 
+			k44:"bb",
+			barba:"papa",
+			k1:"k1",
+			k2:"k2",
+			k3:"k3",
+			k4:"k4",
+			k5:"k5",
+			k6:"k6",
+			k7:"k7",
+			k8:"k8",
+			k9:"k9",
+			k10:"k10",
+			k11:"k11",
+			k12:"k12",
+			k13:"k13",
+			k14:"k14"
+			);
 }
--- vars/log.groovy
+++ vars/log.groovy
@@ -8,9 +8,12 @@
 ])
 
 import java.util.concurrent.TimeUnit;
-
 import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.common.AttributesBuilder
+import io.opentelemetry.api.metrics.LongCounter;
+import io.opentelemetry.api.metrics.Meter;
 import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
 import io.opentelemetry.context.propagation.ContextPropagators;
 import io.opentelemetry.exporter.otlp.metrics.OtlpGrpcMetricExporter;
@@ -22,81 +25,64 @@ import io.opentelemetry.sdk.resources.Resource;
 import io.opentelemetry.sdk.trace.SdkTracerProvider;
 import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
 import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
-import io.opentelemetry.api.OpenTelemetry;
-import io.opentelemetry.api.common.AttributeKey;
-import io.opentelemetry.api.common.Attributes;
-import io.opentelemetry.api.metrics.LongCounter;
-import io.opentelemetry.api.metrics.LongHistogram;
-import io.opentelemetry.api.metrics.Meter;
-import io.opentelemetry.api.trace.Span;
-import io.opentelemetry.api.trace.StatusCode;
-import io.opentelemetry.api.trace.Tracer;
-import io.opentelemetry.context.Scope;
 import io.grpc.*
 
-public class ExampleConfiguration {
+class Otel {
 
-	static OpenTelemetry initOpenTelemetry(Map config = [:]) {
-		// Export traces to Jaeger over OTLP
-	    OtlpGrpcSpanExporter jaegerOtlpExporter =
-	        OtlpGrpcSpanExporter.builder()
-	            .setEndpoint(config.jaegerEndpoint)
-	            .setTimeout(30, TimeUnit.SECONDS)
-	            .build();
-	    
-	    OtlpGrpcMetricExporter metricOtlp =
-	    		OtlpGrpcMetricExporter.builder()
-		            .setEndpoint(config.jaegerEndpoint)
-		            .setTimeout(30, TimeUnit.SECONDS)
-		            .build();
-	    
-		Resource resource = Resource.getDefault().merge(Resource.create(Attributes.of(ResourceAttributes.SERVICE_NAME, config.servicename)));
+	SdkTracerProvider sdkTracerProvider;
+	SdkMeterProvider sdkMeterProvider;
+	LongCounter counter;
+	
+	def Otel(Map config = [:]){
+		 
+		
+		OtlpGrpcSpanExporter spanOtlpExporter = OtlpGrpcSpanExporter.builder()
+				.setEndpoint(config.endpoint)
+				.setTimeout(30, TimeUnit.SECONDS)
+				.build();
+		
+		OtlpGrpcMetricExporter metricOtlpExporter =	OtlpGrpcMetricExporter.builder()
+				.setEndpoint(config.endpoint)
+				.setTimeout(30, TimeUnit.SECONDS)
+				.build();
+		
+		Resource resource = Resource.getDefault()
+				.merge(Resource.create(Attributes.of(ResourceAttributes.SERVICE_NAME, "otel-cli-java")));
 
-				SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder()
-				  .addSpanProcessor(BatchSpanProcessor.builder(jaegerOtlpExporter).build())
-				  .setResource(resource)
-				  .build();
+		sdkTracerProvider = SdkTracerProvider.builder()
+				.addSpanProcessor(BatchSpanProcessor.builder(spanOtlpExporter).build())
+				.setResource(resource).build();
 
-				SdkMeterProvider sdkMeterProvider = SdkMeterProvider.builder()
-				  .registerMetricReader(PeriodicMetricReader.builder(metricOtlp).build())
-				  .setResource(resource)
-				  .build();
+		sdkMeterProvider = SdkMeterProvider.builder()
+				.registerMetricReader(PeriodicMetricReader.builder(metricOtlpExporter).build())
+				.setResource(resource).build();
 
-				OpenTelemetry openTelemetry = OpenTelemetrySdk.builder()
-				  .setTracerProvider(sdkTracerProvider)
-				  .setMeterProvider(sdkMeterProvider)
-				  .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
-				  .buildAndRegisterGlobal();
-		return openTelemetry;
+		OpenTelemetry openTelemetry = OpenTelemetrySdk.builder()
+				.setTracerProvider(sdkTracerProvider)
+				.setMeterProvider(sdkMeterProvider)
+				.setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
+				.buildAndRegisterGlobal();
+		
+		Meter meter = openTelemetry.meterBuilder("instrumentation-library-name")
+				.setInstrumentationVersion("1.0.0")
+				.build();
+
+		counter = meter
+				.counterBuilder(config.counter)
+				.setDescription(config.counter)
+				.setUnit("1")
+				.build();
 	}
-}
-
-def otelcli(Map config = [:]){
-    println("this is endpoint: ${config.endpoint} this is ${config.servicename}")
-    OpenTelemetry openTelemetry = ExampleConfiguration.initOpenTelemetry(jaegerEndpoint:config.endpoint, servicename:config.servicename);
-
-    Tracer tracer = openTelemetry.getTracer("scope");
-	Meter meter = openTelemetry.getMeter("scope");
-	LongCounter counter = meter.counterBuilder("example_counter").build();
-	LongHistogram histogram = meter.histogramBuilder("super_timer").ofLongs().setUnit("ms").build();
-
-    for (int i = 0; i < 25; i++) {
-		long startTime = System.currentTimeMillis();
-	    Span exampleSpan = tracer.spanBuilder("otel-jenkins").startSpan();
-		Scope scope = exampleSpan.makeCurrent();
+	
+	def meterCounter(Map config = [:]) {
 		try {
-			counter.add(1);
-			exampleSpan.addEvent("Event 0");
-			exampleSpan.setAttribute("good", "true");
-			Thread.sleep(100);
+			AttributesBuilder attr = Attributes.builder();
+			config.each{entry -> attr.put(AttributeKey.stringKey(entry.key), entry.value);}
+			counter.add(1, attr.build());
+		} catch(Exception ex){
+			
 		} finally {
-        	histogram.record(System.currentTimeMillis() - startTime);
-			scope.close();
-        	exampleSpan.end();
-			println("closed all")
-        }
+			sdkMeterProvider.close();
+		}
 	}
-      
 }
-
-
