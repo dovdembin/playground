b3b777ba50f3b8f68221af4e3858ee0d194bd6e1
Dov_Dembin <Dov.Dembin@Dell.com>
2023-02-21 14:59:43 +0200

    e

--- vars/log.groovy
+++ vars/log.groovy
@@ -25,70 +25,52 @@ import io.opentelemetry.sdk.trace.SdkTracerProvider;
 import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
 import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
 
-
-public class Otel {
-
-	SdkTracerProvider sdkTracerProvider;
-	SdkMeterProvider sdkMeterProvider;
-	LongCounter counter;
+def meterCounter(Map config = [:]) {
+	OtlpGrpcSpanExporter spanOtlpExporter = OtlpGrpcSpanExporter.builder()
+				.setEndpoint(config.endpoint)
+				.setTimeout(30, TimeUnit.SECONDS)
+				.build();
+		
+	OtlpGrpcMetricExporter metricOtlpExporter =	OtlpGrpcMetricExporter.builder()
+			.setEndpoint(config.endpoint)
+			.setTimeout(30, TimeUnit.SECONDS)
+			.build();
 	
-	def Otel(Map config = [:]){
-		OtlpGrpcSpanExporter spanOtlpExporter = OtlpGrpcSpanExporter.builder()
-				.setEndpoint(config.endpoint)
-				.setTimeout(30, TimeUnit.SECONDS)
-				.build();
-		
-		OtlpGrpcMetricExporter metricOtlpExporter =	OtlpGrpcMetricExporter.builder()
-				.setEndpoint(config.endpoint)
-				.setTimeout(30, TimeUnit.SECONDS)
-				.build();
-		
-		Resource resource = Resource.getDefault()
-				.merge(Resource.create(Attributes.of(ResourceAttributes.SERVICE_NAME, "otel-cli-java")));
+	Resource resource = Resource.getDefault()
+			.merge(Resource.create(Attributes.of(ResourceAttributes.SERVICE_NAME, "otel-cli-java")));
 
-		sdkTracerProvider = SdkTracerProvider.builder()
-				.addSpanProcessor(BatchSpanProcessor.builder(spanOtlpExporter).build())
-				.setResource(resource).build();
+	SdkTracerProvider sdkTracerProvider = SdkTracerProvider.builder()
+			.addSpanProcessor(BatchSpanProcessor.builder(spanOtlpExporter).build())
+			.setResource(resource).build();
 
-		sdkMeterProvider = SdkMeterProvider.builder()
-				.registerMetricReader(PeriodicMetricReader.builder(metricOtlpExporter).build())
-				.setResource(resource).build();
+	SdkMeterProvider sdkMeterProvider = SdkMeterProvider.builder()
+			.registerMetricReader(PeriodicMetricReader.builder(metricOtlpExporter).build())
+			.setResource(resource).build();
 
-		OpenTelemetry openTelemetry = OpenTelemetrySdk.builder()
-				.setTracerProvider(sdkTracerProvider)
-				.setMeterProvider(sdkMeterProvider)
-				.setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
-				.buildAndRegisterGlobal();
-		
-		Meter meter = openTelemetry.meterBuilder("instrumentation-library-name")
-				.setInstrumentationVersion("1.0.0")
-				.build();
+	OpenTelemetry openTelemetry = OpenTelemetrySdk.builder()
+			.setTracerProvider(sdkTracerProvider)
+			.setMeterProvider(sdkMeterProvider)
+			.setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
+			.buildAndRegisterGlobal();
+	
+	Meter meter = openTelemetry.meterBuilder("instrumentation-library-name")
+			.setInstrumentationVersion("1.0.0")
+			.build();
 
-		counter = meter
+	LongCounter counter = meter
 				.counterBuilder(config.counter)
 				.setDescription(config.counter)
 				.setUnit("1")
 				.build();
-	}
 
-	def closeMeterConnection() {
-		sdkMeterProvider.close();
-		sdkMeterProvider.shutdown();
+	Otel otel = new Otel(endpoint:env.OTEL_EXPORTER_OTLP_ENDPOINT, counter:"tridevlab.test-counter");
+	config.remove("endpoint");
+	config.remove("counter");
+	AttributesBuilder attr = Attributes.builder();
+	for (entry in config) {
+		attr.put(entry.getKey(), entry.getValue());
 	}
-}
-
-def meterCounter(Map config = [:]) {
-	try {
-		Otel otel = new Otel(endpoint:env.OTEL_EXPORTER_OTLP_ENDPOINT, counter:"tridevlab.test-counter");
-		config.remove("endpoint");
-		config.remove("counter");
-		AttributesBuilder attr = Attributes.builder();
-		for (entry in config) {
-			attr.put(entry.getKey(), entry.getValue());
-		}
-		otel.counter.add(1, attr.build());
-		otel.closeMeterConnection();
-	}  finally {
+	otel.counter.add(1, attr.build());
 		
-	}
+	
 }
