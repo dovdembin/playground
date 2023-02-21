dd617f37df75b850a767fe4f9fc34e2cedc4a2f5
Dov_Dembin <Dov.Dembin@Dell.com>
2023-02-21 14:13:32 +0200

    dd

--- vars/log.groovy
+++ vars/log.groovy
@@ -3,8 +3,7 @@
    @Grab(group='io.opentelemetry', module='opentelemetry-api', version='1.23.1'),
    @Grab(group='io.opentelemetry', module='opentelemetry-sdk', version='1.23.1'),
    @Grab(group='io.opentelemetry', module='opentelemetry-exporter-otlp', version='1.23.1'),
-   @Grab(group='io.opentelemetry', module='opentelemetry-semconv', version='1.23.1-alpha', scope='runtime'),
-   @Grab(group='io.grpc', module='grpc-stub', version='1.53.0')
+   @Grab(group='io.opentelemetry', module='opentelemetry-semconv', version='1.23.1-alpha', scope='runtime')
 ])
 
 import java.util.concurrent.TimeUnit;
