3215e5df163205712105293432900f0306430bf9
Dov_Dembin <Dov.Dembin@Dell.com>
2023-02-22 20:11:51 +0200

    ff

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,3 +1,4 @@
+@GrabConfig(systemClassLoader=true)
 @Grab(group='io.grpc', module='grpc-stub', version='1.53.0')
 @Grab(group='io.opentelemetry', module='opentelemetry-bom', version='1.23.1', type='pom')
 @Grab(group='io.opentelemetry', module='opentelemetry-api', version='1.23.1')
