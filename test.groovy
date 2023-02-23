f46c50665ddc367f07d981bcf4d77d0540520d7d
Dov_Dembin <Dov.Dembin@Dell.com>
2023-02-23 09:38:45 +0200

    dd

--- test.groovy
+++ test.groovy
@@ -5,32 +5,36 @@
 stage("Running Stage: ") {
 
 
-    def map = [
-        endpoint:env.OTEL_EXPORTER_OTLP_ENDPOINT, 
-        counter:"tridevlab.test-counter", 
-        "test.name":"aa", 
-        "test.bpt-suite":"bb",
-        "test.bpt-merge-candidate":"cc",
-        "test.duration":3456,
-        "test.source":"ee",
-        "test.agent-name":"moshe",
-        "test.ibid":"ff",
-        "test.program-branch":"gg",
-        "test.flavor":"hh",
-        "test.github.pr.id":"ii",
-        "test.github.pr.url":"jj",
-        "test.github.pr.author.login":"kk",
-        "test.github.pr.author.email":"ll",
-        "test.github.source-branch":"mm",
-        "test.github.target-branch":"nn",
-        "test.package-url":"oo",
-        "test.slave-name":"oo",
-        "test.jenkins.build-number":16,
-        "test.jenkins.build-url":"gdg",
-        "test.dingo.unique-id":"eee",
-        "test.sequential.appliance":"ttt",
-        "test.cyc-test":"r5",
-        "test.status":"44re"
-        ]
-    log.meterCounter(map);
+
+    dir(utils) {
+        sh 'java -jar --counter="tridevlab.test-counter" --endpoint=env.OTEL_EXPORTER_OTLP_ENDPOINT -a "test.name"="aa"'
+    }
+    // def map = [
+    //     endpoint:env.OTEL_EXPORTER_OTLP_ENDPOINT, 
+    //     counter:"tridevlab.test-counter", 
+    //     "test.name":"aa", 
+    //     "test.bpt-suite":"bb",
+    //     "test.bpt-merge-candidate":"cc",
+    //     "test.duration":3456,
+    //     "test.source":"ee",
+    //     "test.agent-name":"moshe",
+    //     "test.ibid":"ff",
+    //     "test.program-branch":"gg",
+    //     "test.flavor":"hh",
+    //     "test.github.pr.id":"ii",
+    //     "test.github.pr.url":"jj",
+    //     "test.github.pr.author.login":"kk",
+    //     "test.github.pr.author.email":"ll",
+    //     "test.github.source-branch":"mm",
+    //     "test.github.target-branch":"nn",
+    //     "test.package-url":"oo",
+    //     "test.slave-name":"oo",
+    //     "test.jenkins.build-number":16,
+    //     "test.jenkins.build-url":"gdg",
+    //     "test.dingo.unique-id":"eee",
+    //     "test.sequential.appliance":"ttt",
+    //     "test.cyc-test":"r5",
+    //     "test.status":"44re"
+    //     ]
+     
 }
