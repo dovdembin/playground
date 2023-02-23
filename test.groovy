9d003ec34497f6843d26d2b96ce7d07d9304b6cb
Dov_Dembin <Dov.Dembin@Dell.com>
2023-02-23 20:02:56 +0200

    ssss

--- test.groovy
+++ test.groovy
@@ -13,7 +13,7 @@ pipeline {
                 git 'https://github.com/dovdembin/otelcli.git'
 
                 // Run Maven on a Unix agent.
-                sh "mvn -DskipTests=true clean package"
+                sh "mvn -DskipTests=true clean package shade:shade"
 
                 // To run Maven on a Windows agent, use
                 // bat "mvn -Dmaven.test.failure.ignore=true clean package"
