08963b5859ed57a998dd3af1c7b523fb582e50fb
Dov_Dembin <Dov.Dembin@Dell.com>
2023-02-23 17:11:31 +0200

    sdd

--- test.groovy
+++ test.groovy
@@ -1,47 +1,84 @@
-@Library("shared-library") _
+pipeline {
+    agent any
+
+    tools {
+        // Install the Maven version configured as "M3" and add it to the path.
+        maven "M3"
+    }
+
+    stages {
+        stage('Build') {
+            steps {
+                // Get some code from a GitHub repository
+                git 'https://github.com/dovdembin/otelcli.git'
+
+                // Run Maven on a Unix agent.
+                sh "mvn -Dmaven.test.failure.ignore=true clean package"
+
+                // To run Maven on a Windows agent, use
+                // bat "mvn -Dmaven.test.failure.ignore=true clean package"
+            }
+
+            post {
+                // If Maven was able to run the tests, even if some of the test
+                // failed, record the test results and archive the jar file.
+                success {
+                    junit '**/target/surefire-reports/TEST-*.xml'
+                    archiveArtifacts 'target/*.jar'
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+// @Library("shared-library") _
  
 
 
- node {  
-    stage('Build') { 
+//  node {  
+//     stage('Build') { 
         
-        // sh(script: "java -jar otel.jar -e http://172.30.48.1:4317 -c tridevlab.test-counter", label: "verify_content.sh") 
-        git branch: 'main', url: 'https://github.com/dovdembin/otelcli.git'
+//         // sh(script: "java -jar otel.jar -e http://172.30.48.1:4317 -c tridevlab.test-counter", label: "verify_content.sh") 
+//         git branch: 'main', url: 'https://github.com/dovdembin/otelcli.git'
 
-                // Run Maven on a Unix agent.
-        withMaven {
-            sh "mvn clean verify"
-        } // withMa
-    }
-    // def map = [
-    //     endpoint:env.OTEL_EXPORTER_OTLP_ENDPOINT, 
-    //     counter:"tridevlab.test-counter", 
-    //     "test.name":"aa", 
-    //     "test.bpt-suite":"bb",
-    //     "test.bpt-merge-candidate":"cc",
-    //     "test.duration":3456,
-    //     "test.source":"ee",
-    //     "test.agent-name":"moshe",
-    //     "test.ibid":"ff",
-    //     "test.program-branch":"gg",
-    //     "test.flavor":"hh",
-    //     "test.github.pr.id":"ii",
-    //     "test.github.pr.url":"jj",
-    //     "test.github.pr.author.login":"kk",
-    //     "test.github.pr.author.email":"ll",
-    //     "test.github.source-branch":"mm",
-    //     "test.github.target-branch":"nn",
-    //     "test.package-url":"oo",
-    //     "test.slave-name":"oo",
-    //     "test.jenkins.build-number":16,
-    //     "test.jenkins.build-url":"gdg",
-    //     "test.dingo.unique-id":"eee",
-    //     "test.sequential.appliance":"ttt",
-    //     "test.cyc-test":"r5",
-    //     "test.status":"44re"
-    //     ]
-    // log.meterCounter(map);
-}
+//                 // Run Maven on a Unix agent.
+//         withMaven {
+//             sh "mvn clean verify"
+//         } // withMa
+//     }
+//     // def map = [
+//     //     endpoint:env.OTEL_EXPORTER_OTLP_ENDPOINT, 
+//     //     counter:"tridevlab.test-counter", 
+//     //     "test.name":"aa", 
+//     //     "test.bpt-suite":"bb",
+//     //     "test.bpt-merge-candidate":"cc",
+//     //     "test.duration":3456,
+//     //     "test.source":"ee",
+//     //     "test.agent-name":"moshe",
+//     //     "test.ibid":"ff",
+//     //     "test.program-branch":"gg",
+//     //     "test.flavor":"hh",
+//     //     "test.github.pr.id":"ii",
+//     //     "test.github.pr.url":"jj",
+//     //     "test.github.pr.author.login":"kk",
+//     //     "test.github.pr.author.email":"ll",
+//     //     "test.github.source-branch":"mm",
+//     //     "test.github.target-branch":"nn",
+//     //     "test.package-url":"oo",
+//     //     "test.slave-name":"oo",
+//     //     "test.jenkins.build-number":16,
+//     //     "test.jenkins.build-url":"gdg",
+//     //     "test.dingo.unique-id":"eee",
+//     //     "test.sequential.appliance":"ttt",
+//     //     "test.cyc-test":"r5",
+//     //     "test.status":"44re"
+//     //     ]
+//     // log.meterCounter(map);
+// }
 
 
  
