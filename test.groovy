7184fa8b30a78c619d0580ee64a344268115f68b
Dov_Dembin <Dov.Dembin@Dell.com>
2023-02-23 12:55:50 +0200

    ss

--- test.groovy
+++ test.groovy
@@ -3,38 +3,38 @@
  
 
 try {
-    // def command="java -jar otel.jar -e ${env.OTEL_EXPORTER_OTLP_ENDPOINT} -c tridevlab.test-counter"
-    // println command
-    // res = sh(script: command, returnStdout: true, label: "Run Dingo Completion Command").trim()
+    def command="java -jar otel.jar -e ${env.OTEL_EXPORTER_OTLP_ENDPOINT} -c tridevlab.test-counter"
+    println command
+    res = sh(script: command, returnStdout: true, label: "Run Dingo Completion Command").trim()
 
-def map = [
-        endpoint:env.OTEL_EXPORTER_OTLP_ENDPOINT, 
-        counter:"tridevlab.test-counter", 
-        "test.name":"aa", 
-        "test.bpt-suite":"bb",
-        "test.bpt-merge-candidate":"cc",
-        "test.duration":3456,
-        "test.source":"ee",
-        "test.agent-name":"moshe",
-        "test.ibid":"ff",
-        "test.program-branch":"gg",
-        "test.flavor":"hh",
-        "test.github.pr.id":"ii",
-        "test.github.pr.url":"jj",
-        "test.github.pr.author.login":"kk",
-        "test.github.pr.author.email":"ll",
-        "test.github.source-branch":"mm",
-        "test.github.target-branch":"nn",
-        "test.package-url":"oo",
-        "test.slave-name":"oo",
-        "test.jenkins.build-number":16,
-        "test.jenkins.build-url":"gdg",
-        "test.dingo.unique-id":"eee",
-        "test.sequential.appliance":"ttt",
-        "test.cyc-test":"r5",
-        "test.status":"44re"
-        ]
-    log.meterCounter(map);
+// def map = [
+//         endpoint:env.OTEL_EXPORTER_OTLP_ENDPOINT, 
+//         counter:"tridevlab.test-counter", 
+//         "test.name":"aa", 
+//         "test.bpt-suite":"bb",
+//         "test.bpt-merge-candidate":"cc",
+//         "test.duration":3456,
+//         "test.source":"ee",
+//         "test.agent-name":"moshe",
+//         "test.ibid":"ff",
+//         "test.program-branch":"gg",
+//         "test.flavor":"hh",
+//         "test.github.pr.id":"ii",
+//         "test.github.pr.url":"jj",
+//         "test.github.pr.author.login":"kk",
+//         "test.github.pr.author.email":"ll",
+//         "test.github.source-branch":"mm",
+//         "test.github.target-branch":"nn",
+//         "test.package-url":"oo",
+//         "test.slave-name":"oo",
+//         "test.jenkins.build-number":16,
+//         "test.jenkins.build-url":"gdg",
+//         "test.dingo.unique-id":"eee",
+//         "test.sequential.appliance":"ttt",
+//         "test.cyc-test":"r5",
+//         "test.status":"44re"
+//         ]
+//     log.meterCounter(map);
 } catch (Exception e) {
          
 }
