873f6a78485254d37e8e0cccec697143c511549f
Dov_Dembin <Dov.Dembin@Dell.com>
2023-02-27 02:17:24 +0200

    ss

--- test.groovy
+++ test.groovy
@@ -50,34 +50,34 @@ pipeline {
 //             sh "mvn clean verify"
 //         } // withMa
 //     }
-//     // def map = [
-//     //     endpoint:env.OTEL_EXPORTER_OTLP_ENDPOINT, 
-//     //     counter:"tridevlab.test-counter", 
-//     //     "test.name":"aa", 
-//     //     "test.bpt-suite":"bb",
-//     //     "test.bpt-merge-candidate":"cc",
-//     //     "test.duration":3456,
-//     //     "test.source":"ee",
-//     //     "test.agent-name":"moshe",
-//     //     "test.ibid":"ff",
-//     //     "test.program-branch":"gg",
-//     //     "test.flavor":"hh",
-//     //     "test.github.pr.id":"ii",
-//     //     "test.github.pr.url":"jj",
-//     //     "test.github.pr.author.login":"kk",
-//     //     "test.github.pr.author.email":"ll",
-//     //     "test.github.source-branch":"mm",
-//     //     "test.github.target-branch":"nn",
-//     //     "test.package-url":"oo",
-//     //     "test.slave-name":"oo",
-//     //     "test.jenkins.build-number":16,
-//     //     "test.jenkins.build-url":"gdg",
-//     //     "test.dingo.unique-id":"eee",
-//     //     "test.sequential.appliance":"ttt",
-//     //     "test.cyc-test":"r5",
-//     //     "test.status":"44re"
-//     //     ]
-//     // log.meterCounter(map);
+    def map = [
+        endpoint:env.OTEL_EXPORTER_OTLP_ENDPOINT, 
+        counter:"tridevlab.test-counter", 
+        "test.name":"aa", 
+        "test.bpt-suite":"bb",
+        "test.bpt-merge-candidate":"cc",
+        "test.duration":3456,
+        "test.source":"ee",
+        "test.agent-name":"moshe",
+        "test.ibid":"ff",
+        "test.program-branch":"gg",
+        "test.flavor":"hh",
+        "test.github.pr.id":"ii",
+        "test.github.pr.url":"jj",
+        "test.github.pr.author.login":"kk",
+        "test.github.pr.author.email":"ll",
+        "test.github.source-branch":"mm",
+        "test.github.target-branch":"nn",
+        "test.package-url":"oo",
+        "test.slave-name":"oo",
+        "test.jenkins.build-number":16,
+        "test.jenkins.build-url":"gdg",
+        "test.dingo.unique-id":"eee",
+        "test.sequential.appliance":"ttt",
+        "test.cyc-test":"r5",
+        "test.status":"44re"
+        ]
+    log.meterCounter(map);
 // }
 
 
---
+++ vars/otel.groovy
@@ -0,0 +1,64 @@
+
+@Grab(group='io.opentelemetry', module='opentelemetry-bom', version='1.23.1', type='pom')
+@Grab(group='io.opentelemetry', module='opentelemetry-api', version='1.23.1')
+@Grab(group='io.opentelemetry', module='opentelemetry-sdk', version='1.23.1')
+@Grab(group='io.opentelemetry', module='opentelemetry-exporter-otlp', version='1.23.1')
+@Grab(group='io.opentelemetry', module='opentelemetry-semconv', version='1.23.1-alpha', scope='runtime')
+
+
+import java.util.concurrent.TimeUnit;
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.common.AttributesBuilder
+import io.opentelemetry.api.metrics.LongCounter;
+import io.opentelemetry.api.metrics.Meter;
+import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
+import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.exporter.otlp.metrics.OtlpGrpcMetricExporter;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.metrics.SdkMeterProvider;
+import io.opentelemetry.sdk.metrics.export.PeriodicMetricReader;
+import io.opentelemetry.sdk.resources.Resource;
+import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
+import static io.opentelemetry.semconv.resource.attributes.ResourceAttributes.SERVICE_NAME;
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
+import java.time.Duration;
+
+
+
+def meterCounter(Map config = [:]) {
+	println env.OTEL_EXPORTER_OTLP_ENDPOINT
+	Resource resource =
+        Resource.getDefault()
+            .merge(Resource.builder().put("dd", "OtlpExporterExample").build());
+
+    OpenTelemetrySdk openTelemetrySdk =
+        OpenTelemetrySdk.builder()
+            .setMeterProvider(
+                SdkMeterProvider.builder()
+                    .setResource(resource)
+                    .registerMetricReader(
+                        PeriodicMetricReader.builder(OtlpGrpcMetricExporter.getDefault())
+                            .setInterval(Duration.ofMillis(1000))
+                            .build())
+                    .build())
+            .buildAndRegisterGlobal();
+
+	Meter meter = openTelemetrySdk.meterBuilder("instrumentation-library-name")
+			.setInstrumentationVersion("1.0.0")
+			.build();
+
+	LongCounter counter = meter
+			.counterBuilder(config.counter)
+			.setDescription(config.counter)
+			.setUnit("1")
+			.build();
+	AttributesBuilder attr = Attributes.builder();
+	for (entry in config) {
+		attr.put(entry.key, entry.value);
+	}
+	counter.add(1, attr.build());
+	openTelemetrySdk.getSdkMeterProvider().close();
+}
