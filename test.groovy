36a39c34e586988b957aa59ed0be91eb1ed92e1a
Dov_Dembin <Dov.Dembin@Dell.com>
2023-02-27 09:40:28 +0200

    sss

--- test.groovy
+++ test.groovy
@@ -37,12 +37,13 @@
 
 // @Library("shared-library") _
  
-node {  
-    stage('Build') { 
-        tools {
+node { 
+    tools {
         // Install the Maven version configured as "M3" and add it to the path.
         maven "M3"
-    }
+    } 
+    stage('Build') { 
+        
           
                 // Get some code from a GitHub repository
                 git branch: 'main', url: 'https://github.com/dovdembin/otelcli.git'
