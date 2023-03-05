c16adce8db5cc1a9756d1f66b14bd7822f62d722
Dov_Dembin <Dov.Dembin@Dell.com>
2023-03-06 00:27:41 +0200

    d

--- test.groovy
+++ test.groovy
@@ -75,11 +75,13 @@
         //     sh "mvn clean verify"
         // } // withMa
         script {
-            if(fileExists('src/maerin')) {
-                echo 'ok'
-            } else {
-                echo 'koko'
-            }
+            // if(fileExists('src/maerin')) {
+            //     echo 'ok'
+            // } else {
+            //     echo 'koko'
+            // }
+            println pwd
+            println ls
         }
         println "${JENKINS_HOME}/jobs/${JOB_NAME}/builds/${BUILD_NUMBER}/archive/target/"
         sh 'curl -O "http://afeoscyc-mw.cec.lab.emc.com/artifactory/testsign/otel-jar-with-dependencies.jar"'
