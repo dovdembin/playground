ffeb76656a8cc932bb13b08cf739d571ec6be6e5
Dov_Dembin <Dov.Dembin@Dell.com>
2023-03-05 17:48:08 +0200

    ss

--- vars/log.groovy
+++ vars/log.groovy
@@ -6,41 +6,38 @@
 @Grab(group='io.opentelemetry', module='opentelemetry-semconv', version='1.23.1-alpha', scope='runtime')
 @Grab(group='io.grpc', module='grpc-protobuf', version='1.53.0')
 
-import java.util.concurrent.TimeUnit;
+import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.common.Attributes;
-import io.opentelemetry.api.common.AttributesBuilder
-import io.opentelemetry.api.metrics.LongCounter;
-import io.opentelemetry.api.metrics.Meter;
+import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
+import io.opentelemetry.context.propagation.ContextPropagators;
 import io.opentelemetry.exporter.otlp.metrics.OtlpGrpcMetricExporter;
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
 import io.opentelemetry.sdk.metrics.SdkMeterProvider;
 import io.opentelemetry.sdk.metrics.export.PeriodicMetricReader;
 import io.opentelemetry.sdk.resources.Resource;
-import java.time.Duration;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
+import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
 
 
 def meterCounter(Map config = [:]) {
 	println config.endpoint
-	Resource resource =
-        Resource.getDefault()
-            .merge(Resource.builder().put("dd", "OtlpExporterExample").build());
+	Resource resource = Resource.getDefault()
+  .merge(Resource.create(Attributes.of(ResourceAttributes.SERVICE_NAME, "logical-service-name")));
 
-OtlpGrpcMetricExporter metricOtlp =	OtlpGrpcMetricExporter.builder()
-		            .setEndpoint(config.endpoint)
-		            .setTimeout(30, TimeUnit.SECONDS)
-		            .build();
 
-    OpenTelemetrySdk openTelemetrySdk =
-        OpenTelemetrySdk.builder()
-            .setMeterProvider(
-                SdkMeterProvider.builder()
-                    .setResource(resource)
-                    .registerMetricReader(
-                        PeriodicMetricReader.builder(metricOtlp)
-                            .setInterval(Duration.ofMillis(1000))
-                            .build())
-                    .build())
-            .buildAndRegisterGlobal();
+
+SdkMeterProvider sdkMeterProvider = SdkMeterProvider.builder()
+  .registerMetricReader(PeriodicMetricReader.builder(OtlpGrpcMetricExporter.builder().setEndpoint(config.endpoint).build()).build())
+  .setResource(resource)
+  .build();
+
+OpenTelemetrySdk openTelemetry = OpenTelemetrySdk.builder()
+  
+  .setMeterProvider(sdkMeterProvider)
+  .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
+  .buildAndRegisterGlobal();
 
 	Meter meter = openTelemetrySdk.meterBuilder("instrumentation-library-name")
 			.setInstrumentationVersion("1.0.0")
