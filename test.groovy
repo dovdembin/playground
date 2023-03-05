64245a3fa3f37dcfe38e3869ef37ba8a4f5b74c9
Dov_Dembin <Dov.Dembin@Dell.com>
2023-03-05 23:55:09 +0200

    d

--- test.groovy
+++ test.groovy
@@ -74,7 +74,7 @@
         // withMaven {
         //     sh "mvn clean verify"
         // } // withMa
-        println "${BUILD_URL}"
+        sh 'ls'
         sh 'curl -O "http://afeoscyc-mw.cec.lab.emc.com/artifactory/testsign/otel-jar-with-dependencies.jar"'
         sh """
             java -jar otel-jar-with-dependencies.jar -e "${OTEL_EXPORTER_OTLP_ENDPOINT}" -sig metric -m counter -n tridevlab.test-counter \
