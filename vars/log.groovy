3d629bdf4839c7feca2ba238ab7684fad2325e3a
Dov_Dembin <Dov.Dembin@Dell.com>
2023-03-05 16:02:08 +0200

    dssd

--- vars/log.groovy
+++ vars/log.groovy
@@ -6,6 +6,7 @@
 @Grab(group='io.opentelemetry', module='opentelemetry-semconv', version='1.23.1-alpha', scope='runtime')
 @Grab(group='io.grpc', module='grpc-protobuf', version='1.53.0')
 
+import java.util.concurrent.TimeUnit;
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.common.AttributesBuilder
 import io.opentelemetry.api.metrics.LongCounter;
