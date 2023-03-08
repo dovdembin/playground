b121e4d266bee9ba9d7f269155640240ae45c94f
Dov_Dembin <Dov.Dembin@Dell.com>
2023-03-08 16:12:30 +0200

    ff

--- test.groovy
+++ test.groovy
@@ -74,38 +74,15 @@
         // withMaven {
         //     sh "mvn clean verify"
         // } // withMa
-        sh 'ls'
-        println "${JENKINS_HOME}/jobs/${JOB_NAME}/builds/${BUILD_NUMBER}/archive/target/"
-        dir("${JENKINS_HOME}/jobs/${JOB_NAME}/builds/${BUILD_NUMBER}/archive/target/"){
-            sh 'curl -k -O "https://afeoscyc-mw.cec.lab.emc.com/artifactory/testsign/otel-jar-with-dependencies.jar"'
-            sh 'ls'
-            sh """
-                java -jar otel-jar-with-dependencies.jar -e "${OTEL_EXPORTER_OTLP_ENDPOINT}" -sig metric -m counter -n tridevlab.test-counter \
-                -a test.name="${config_base_name}" \
-                -a test.bpt-suite="${main_envs["bpt_suite"]}" \
-                -a test.bpt-merge-candidate="${main_envs["bpt_merge_candidate"]}" \
-                -i test.duration="${duration}" \
-                -a test.source="${TRIGGERED_BY}" \
-                -a test.agent-name="${env.HOST_NAME}" \
-                -a test.ibid="${IBID}" \
-                -a test.program-branch="${PROGRAM_BRANCH}" \
-                -a test.flavor="${FLAVOR}" \
-                -a test.github.pr.id="${GIT_PR_ID}" \
-                -a test.github.pr.url="${GIT_PR_LINK}" \
-                -a test.github.pr.author.login="${GIT_PR_AUTHOR_LOGIN}" \
-                -a test.github.pr.author.email="${GIT_PR_AUTHOR_EMAIL}" \
-                -a test.github.source-branch="${GIT_PR_SOURCE_BRANCH}" \
-                -a test.github.target-branch="${GIT_PR_TARGET_BRANCH}" \
-                -a test.package-url="${PACKAGE_URL}" \
-                -a test.slave-name="${slave_name}" \
-                -i test.jenkins.build-number="${BUILD_NUMBER}" \
-                -a test.jenkins.build-url="${BUILD_URL}" \
-                -a test.dingo.unique-id="${UNIQUE_ID}" \
-                -a test.sequential.appliance="${SEQUENTIAL_APPLIANCE}" \
-                -a test.cyc-test="${CYC_TEST}" \
-                -a test.status=${end_status}""
-            """
-    }
+                bpt_labels="--lg 1 -l @indus1\|@indus2\|@indus3,MLK"
+                println "${JENKINS_HOME}/jobs/${JOB_NAME}/builds/${BUILD_NUMBER}/archive/target/"
+                dir("${JENKINS_HOME}/jobs/${JOB_NAME}/builds/${BUILD_NUMBER}/archive/target/"){
+                sh 'curl -k -O "https://afeoscyc-mw.cec.lab.emc.com/artifactory/testsign/otel-jar-with-dependencies.jar"'
+                sh """
+                    java -jar otel-jar-with-dependencies.jar -e "${OTEL_EXPORTER_OTLP_ENDPOINT}" -sig metric -m counter -n tridevlab.test-counter \
+                    -l test.testlabel="${bpt_labels}" 
+                """
+                }
         }
     // def map = [
     //     endpoint:env.OTEL_EXPORTER_OTLP_ENDPOINT, 
