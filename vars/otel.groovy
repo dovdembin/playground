8e0efd8b733c9c95987002a3cdf45911c2bcc062
Dov_Dembin <Dov.Dembin@Dell.com>
2023-03-09 12:22:34 +0200

    adsf

--- vars/otel.groovy
+++ vars/otel.groovy
@@ -1,7 +1,10 @@
 import java.util.regex.Pattern
 
+
 def getLabels(String labels) {
-	def pattern = ~".*-l(.*)"
-	def matcher = labels =~ pattern
-	return matcher[0][1].toString().replace("\\|", ",").trim();
+	if(labels.isEmpty()) {
+		def pattern = ~".*-l(.*)"
+		def matcher = labels =~ pattern
+		return matcher[0][1].toString().replace("\\|", ",").trim();
+	}
 }
