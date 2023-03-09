b60cbe600ec459b7058dc0d9028d62791b035fac
Dov_Dembin <Dov.Dembin@Dell.com>
2023-03-09 11:30:21 +0200

    d

--- vars/otel.groovy
+++ vars/otel.groovy
@@ -3,5 +3,5 @@ import java.util.regex.Pattern
 def getLabels(String labels) {
 	def pattern = ~".*-l(.*)"
 	def matcher = labels =~ pattern
-	return matcher[0][1].toString().replace("\\|", ",");
+	return matcher[0][1].toString().replace("\\|", ",").trim();
 }
