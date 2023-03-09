ee19dfc41f0bec728a3e1fee8c0c02e2e7499387
Dov_Dembin <Dov.Dembin@Dell.com>
2023-03-09 12:24:29 +0200

    d

--- vars/otel.groovy
+++ vars/otel.groovy
@@ -2,9 +2,11 @@ import java.util.regex.Pattern
 
 
 def getLabels(String labels) {
-	if(labels!-null) {
+	if(labels!=null) {
 		def pattern = ~".*-l(.*)"
 		def matcher = labels =~ pattern
 		return matcher[0][1].toString().replace("\\|", ",").trim();
+	} else {
+		return "noLabel"
 	}
 }
