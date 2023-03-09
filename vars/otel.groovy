dc7678061061ef7ee509214394a3ff9e7e077c8f
Dov_Dembin <Dov.Dembin@Dell.com>
2023-03-09 12:23:38 +0200

    df

--- vars/otel.groovy
+++ vars/otel.groovy
@@ -2,7 +2,7 @@ import java.util.regex.Pattern
 
 
 def getLabels(String labels) {
-	if(labels.isEmpty()) {
+	if(labels!-null) {
 		def pattern = ~".*-l(.*)"
 		def matcher = labels =~ pattern
 		return matcher[0][1].toString().replace("\\|", ",").trim();
