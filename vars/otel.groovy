544e6d32f977438d2a9b228ca253bb2b11bb1037
Dov_Dembin <Dov.Dembin@Dell.com>
2023-03-14 01:17:09 +0200

    ff

--- vars/otel.groovy
+++ vars/otel.groovy
@@ -25,7 +25,7 @@ def getLabels(String text2, String appliance2) {
 	} else {
 		return "noMatch"
 	}
-
+    @NonCPS
 	def res = sh(script:"""
 							curl -s --location 'http://labjungle.devops.xiodrm.lab.emc.com/api/v1/cluster/?name=${appliance2}' \
 							--header 'Authorization: ApiKey cute:9703aa016d613b2b21bbb0e6833c3078c811a5d1' | \
