ac1a1df09f63efecf982472fae1d7d9159219c22
Dov_Dembin <Dov.Dembin@Dell.com>
2023-03-13 23:26:13 +0200

    ss

--- vars/otel.groovy
+++ vars/otel.groovy
@@ -11,7 +11,7 @@ def getLabels(String text, String appliance) {
 							--header 'Authorization: ApiKey cute:9703aa016d613b2b21bbb0e6833c3078c811a5d1' | \
 							jq -r -j '.objects[] | .tags + \",\" + .generation.name'
 						""", returnStdout: true, label: "xpool_allocation")
-	 	return res
+	 	
 		ArrayList ljLabels = res.split(",")
 		ArrayList listlabes = lblList.split(",")
         return listlabes.intersect(ljLabels)
