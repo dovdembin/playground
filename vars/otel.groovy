58dd67b24293c3afb9067e948cb202217506bc02
Dov_Dembin <Dov.Dembin@Dell.com>
2023-03-14 01:16:35 +0200

    d

--- vars/otel.groovy
+++ vars/otel.groovy
@@ -26,14 +26,14 @@ def getLabels(String text2, String appliance2) {
 		return "noMatch"
 	}
 
-	// def res = sh(script:"""
-	// 						curl -s --location 'http://labjungle.devops.xiodrm.lab.emc.com/api/v1/cluster/?name=${appliance2}' \
-	// 						--header 'Authorization: ApiKey cute:9703aa016d613b2b21bbb0e6833c3078c811a5d1' | \
-	// 						jq -r -j '.objects[] | .tags + \",\" + .generation.name'
-	// 					""", returnStdout: true, label: "xpool_allocation")
+	def res = sh(script:"""
+							curl -s --location 'http://labjungle.devops.xiodrm.lab.emc.com/api/v1/cluster/?name=${appliance2}' \
+							--header 'Authorization: ApiKey cute:9703aa016d613b2b21bbb0e6833c3078c811a5d1' | \
+							jq -r -j '.objects[] | .tags + \",\" + .generation.name'
+						""", returnStdout: true, label: "xpool_allocation")
 
-	// ArrayList ljLabels = res.split(",")
-	// ArrayList listlabes = lblList.split(",")
-    // return listlabes.intersect(ljLabels).join(", ")
-	return "asldfkj,kajdf"
+	ArrayList ljLabels = res.split(",")
+	ArrayList listlabes = lblList.split(",")
+    return listlabes.intersect(ljLabels).join(", ")
+	
 }
