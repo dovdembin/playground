ea72c15230575c63d07f87d3e22e9780d11c5a97
Dov_Dembin <Dov.Dembin@Dell.com>
2023-03-13 23:34:38 +0200

    s

--- vars/otel.groovy
+++ vars/otel.groovy
@@ -1,25 +1,21 @@
 def getLabels(String text, String appliance) {
+	
 	def pattern = /.*\s-l\s(.*)/
 	if(text ==~ pattern) {
 		def (word1) = text =~ pattern
 		def lblList = word1[1].toString().replaceAll(/\\\|/, ",").trim()
-        // def  lblList = result[0][1].toString().replaceAll(/\\\|/) { match -> ","  }
-		
-		
-		def res = sh(script:"""
+	} else {
+		return "noMatch"
+	}
+
+	def res = sh(script:"""
 							curl -s --location 'http://labjungle.devops.xiodrm.lab.emc.com/api/v1/cluster/?name=${appliance}' \
 							--header 'Authorization: ApiKey cute:9703aa016d613b2b21bbb0e6833c3078c811a5d1' | \
 							jq -r -j '.objects[] | .tags + \",\" + .generation.name'
 						""", returnStdout: true, label: "xpool_allocation")
-	 	
-		// ArrayList ljLabels = res.split(",")
-		// ArrayList listlabes = lblList.split(",")
-        // return listlabes.intersect(ljLabels)
-		return '["sdaf","adsf"]'
 
-	} else {
-		return "noMatch"
-	}
-	 
+	ArrayList ljLabels = res.split(",")
+	ArrayList listlabes = lblList.split(",")
+    return listlabes.intersect(ljLabels)
 }
 
