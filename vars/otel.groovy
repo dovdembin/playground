733f5f1e3384f6f7dbaabc1e8a8b56b74eccca3e
Dov_Dembin <Dov.Dembin@Dell.com>
2023-03-13 21:59:11 +0200

    dd

--- vars/otel.groovy
+++ vars/otel.groovy
@@ -13,11 +13,11 @@ def getLabels(String text) {
 def getIntersection(labels){
 	ArrayList listlabes = labels.split(",")
 	// println listlabes
-	// String res = sh(script: """
-	// 					curl -s --location 'http://labjungle.devops.xiodrm.lab.emc.com/api/v1/cluster/?name=WK-D0046' \
-	// 					--header 'Authorization: ApiKey cute:9703aa016d613b2b21bbb0e6833c3078c811a5d1' | \
-	// 					jq -r -j '.objects[] | .tags + "," + .generation.name'
-	// 				""", returnStdout: true, label: "xpool_allocation")
+	String res = sh(script: """
+						curl -s --location 'http://labjungle.devops.xiodrm.lab.emc.com/api/v1/cluster/?name=WK-D0046' \
+						--header 'Authorization: ApiKey cute:9703aa016d613b2b21bbb0e6833c3078c811a5d1' | \
+						jq -r -j '.objects[] | .tags + "," + .generation.name'
+					""", returnStdout: true, label: "xpool_allocation")
 	// println res			
 	// ArrayList ljLabels = res.split(",")
 	// return listlabes.intersect(ljLabels)
