7553d03ec809474cb661d6e2806a52a963cb249b
Dov_Dembin <Dov.Dembin@Dell.com>
2023-03-13 23:03:32 +0200

    w

--- vars/otel.groovy
+++ vars/otel.groovy
@@ -1,8 +1,8 @@
 def getLabels(String text, String appliance) {
-	def pattern = /.*\s-l\s(.*)/
-	if(text ==~ pattern) {
-		def (word1) = text =~ pattern
-		def lblList = word1[1].toString().replaceAll(/\\\|/, ",").trim()
+	// def pattern = /.*\s-l\s(.*)/
+	// if(text ==~ pattern) {
+	// 	def (word1) = text =~ pattern
+	// 	def lblList = word1[1].toString().replaceAll(/\\\|/, ",").trim()
 		
 		
 		def res = sh(script:"""
@@ -10,14 +10,14 @@ def getLabels(String text, String appliance) {
 							--header 'Authorization: ApiKey cute:9703aa016d613b2b21bbb0e6833c3078c811a5d1' | \
 							jq -r -j '.objects[] | .tags + \",\" + .generation.name'
 						""", returnStdout: true, label: "xpool_allocation")
-	 	
-		ArrayList ljLabels = res.split(",")
-		ArrayList listlabes = lblList.split(",")
-        listlabes.intersect(ljLabels)
+	 	return res
+		// ArrayList ljLabels = res.split(",")
+		// ArrayList listlabes = lblList.split(",")
+        // listlabes.intersect(ljLabels)
 
-	} else {
-		return "noMatch"
-	}
+	// } else {
+	// 	return "noMatch"
+	// }
 	 
 }
 
