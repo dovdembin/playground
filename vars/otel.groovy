4b4b016fa8fd189b8838330da5d6e6e1b2c214d2
Dov_Dembin <Dov.Dembin@Dell.com>
2023-03-13 22:13:42 +0200

    s

--- vars/otel.groovy
+++ vars/otel.groovy
@@ -4,6 +4,7 @@ def getLabels(String text) {
 	if(text ==~ pattern) {
 		def (word1) = text =~ pattern
 		def lblList = word1[1].toString().replaceAll(/\\\|/, ",").trim()
+		println lblList
 		return getIntersection(lblList)
 	} else {
 		return "noMatch"
@@ -14,7 +15,6 @@ def getLabels(String text) {
 
 ArrayList<String> getIntersection(String labels){
 	ArrayList listlabes = labels.split(",")
-	// println listlabes
 	def res = sh(script: """
 	curl -s --location 'http://labjungle.devops.xiodrm.lab.emc.com/api/v1/cluster/?name=WK-D0046' \
 	--header 'Authorization: ApiKey cute:9703aa016d613b2b21bbb0e6833c3078c811a5d1' | \
