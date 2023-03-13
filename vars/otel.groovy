8f16350b20c837ba2780e1e9b77e0a13bbbf8658
Dov_Dembin <Dov.Dembin@Dell.com>
2023-03-14 01:03:38 +0200

    s

--- vars/otel.groovy
+++ vars/otel.groovy
@@ -3,9 +3,10 @@ def checkLabels(String text, String appliance) {
 	def pattern = /([A-Z][A-Z]-[A-Z]\d\d\d\d)-([A-Z][A-Z]-[A-Z]\d\d\d\d)-TAG/
 	if(appliance ==~ pattern){
 		def (res1) = appliance =~ pattern
-		def list1 = getLabels(text, res1[1].toString())
-		def list2 = getLabels(text, res1[2].toString())
-		return "${list1},${list2}"
+		def list1 = getLabels(text, res1[1])
+		def list2 = getLabels(text, res1[2])
+		def createdList = list1.addAll(list2)
+		return createdList
 	}
 	else {
 		return getLabels(text, appliance)
