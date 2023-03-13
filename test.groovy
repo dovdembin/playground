3af83cd54b1b7359a957fc65d1619dcfdcdf5ef5
Dov_Dembin <Dov.Dembin@Dell.com>
2023-03-14 00:56:03 +0200

    s

--- test.groovy
+++ test.groovy
@@ -118,7 +118,7 @@
     //     ]
     //     println env.OTEL_EXPORTER_OTLP_ENDPOINT
     
-    def config_params = ['xpoolAllocation':'--lg 1 -l EX\\|MLK,PhysicalLG']
+    def config_params = ['xpoolAllocation':'--federation 1,1 -l EX\\|MLK,PhysicalLG']
 
     println otel.checkLabels(config_params['xpoolAllocation'], "WK-D0077-WK-D0661-TAG");
 }
