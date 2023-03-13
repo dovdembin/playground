85558aa5210b8527c89664b6e484f5fdae86ff0e
Dov_Dembin <Dov.Dembin@Dell.com>
2023-03-13 23:02:40 +0200

    s

--- vars/otel.groovy
+++ vars/otel.groovy
@@ -3,22 +3,21 @@ def getLabels(String text, String appliance) {
 	if(text ==~ pattern) {
 		def (word1) = text =~ pattern
 		def lblList = word1[1].toString().replaceAll(/\\\|/, ",").trim()
-		return getIntersection(lblList, appliance)
+		
+		
+		def res = sh(script:"""
+							curl -s --location 'http://labjungle.devops.xiodrm.lab.emc.com/api/v1/cluster/?name=${appliance}' \
+							--header 'Authorization: ApiKey cute:9703aa016d613b2b21bbb0e6833c3078c811a5d1' | \
+							jq -r -j '.objects[] | .tags + \",\" + .generation.name'
+						""", returnStdout: true, label: "xpool_allocation")
+	 	
+		ArrayList ljLabels = res.split(",")
+		ArrayList listlabes = lblList.split(",")
+        listlabes.intersect(ljLabels)
+
 	} else {
 		return "noMatch"
 	}
 	 
 }
 
-
-def getIntersection(String labels, String appliance){
-	ArrayList listlabes = labels.split(",")
-	def res = sh(script:"""
-							curl -s --location 'http://labjungle.devops.xiodrm.lab.emc.com/api/v1/cluster/?name=${appliance}' \
-							--header 'Authorization: ApiKey cute:9703aa016d613b2b21bbb0e6833c3078c811a5d1' | \
-							jq -r -j '.objects[] | .tags + \",\" + .generation.name'
-						""", returnStdout: true, label: "xpool_allocation")
-	 	
-	ArrayList ljLabels = res.split(",")
-	return listlabes.intersect(ljLabels)
-}
