52d99e78b70c690d05fa8496ad910351079a914c
Dov_Dembin <Dov.Dembin@Dell.com>
2023-03-14 01:24:00 +0200

    sadf

--- vars/otel.groovy
+++ vars/otel.groovy
@@ -1,5 +1,5 @@
 
-@NonCPS
+
 def checkLabels(String text, String appliance) {
 	def pattern = /([A-Z][A-Z]-[A-Z]\d\d\d\d)-([A-Z][A-Z]-[A-Z]\d\d\d\d)-TAG/
 	if(appliance ==~ pattern){
@@ -25,7 +25,6 @@ def getLabels(String text2, String appliance2) {
 	} else {
 		return "noMatch"
 	}
-    @NonCPS
 	def res = sh(script:"""
 							curl -s --location 'http://labjungle.devops.xiodrm.lab.emc.com/api/v1/cluster/?name=${appliance2}' \
 							--header 'Authorization: ApiKey cute:9703aa016d613b2b21bbb0e6833c3078c811a5d1' | \
