3f0a4891e3192285206bec91f03b2a55b380e54f
Dov_Dembin <Dov.Dembin@Dell.com>
2023-03-13 22:56:54 +0200

    d

--- vars/otel.groovy
+++ vars/otel.groovy
@@ -14,17 +14,12 @@ def getLabels(String text) {
 
 ArrayList<String> getIntersection(String labels){
 	ArrayList listlabes = labels.split(",")
-	// println listlabes
-	def res = sh(script: """
-	curl -s --location 'http://labjungle.devops.xiodrm.lab.emc.com/api/v1/cluster/?name=WK-D0046' \
-	--header 'Authorization: ApiKey cute:9703aa016d613b2b21bbb0e6833c3078c811a5d1' | \
-	jq -r -j '.objects[] | .tags + \",\" + .generation.name'
-	""", returnStdout: true, label: "xpool_allocation")
+	def res = sh(script:"""
+							curl -s --location 'http://labjungle.devops.xiodrm.lab.emc.com/api/v1/cluster/?name=WK-D0046' \
+							--header 'Authorization: ApiKey cute:9703aa016d613b2b21bbb0e6833c3078c811a5d1' | \
+							jq -r -j '.objects[] | .tags + \",\" + .generation.name'
+						""", returnStdout: true, label: "xpool_allocation")
 	 	
 	ArrayList ljLabels = res.split(",")
 	return listlabes.intersect(ljLabels)
 }
-
-// config_params       = ['xpoolAllocation':'--lg 1 -l indus1\\|indus2\\|indus3,PhysicalLG,powerActionSupported,@Indus_Not_Aligned']
-
-// println getLabels(config_params['xpoolAllocation'])
