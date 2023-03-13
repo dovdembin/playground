1d063dff75611eda39a293e112d0878a20a7119f
Dov_Dembin <Dov.Dembin@Dell.com>
2023-03-13 21:38:30 +0200

    d

--- vars/otel.groovy
+++ vars/otel.groovy
@@ -3,6 +3,7 @@ def getLabels(String text) {
 	if(text ==~ pattern) {
 		def (word1) = text =~ pattern
 		def lblList = word1[1].toString().replaceAll(/\\\|/, ",").trim()
+		print(lblList)
 		return getIntersection(lblList)
 	} else {
 		return "noMatch"
