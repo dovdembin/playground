e7bac8b453b35228a5a5108c74b98aeb25a301d9
Dov_Dembin <Dov.Dembin@Dell.com>
2023-03-13 21:18:17 +0200

    d

--- vars/otel.groovy
+++ vars/otel.groovy
@@ -7,7 +7,7 @@ def getLabels(String text) {
 						curl -s --location 'http://labjungle.devops.xiodrm.lab.emc.com/api/v1/cluster/?name=WK-D0046' \
 						--header 'Authorization: ApiKey cute:9703aa016d613b2b21bbb0e6833c3078c811a5d1' | \
 						jq -r '.objects[] | .tags + "," + .generation.name'
-					""", returnStdout: true, label: "xpool_allocation")
+					""", returnStdout: true, label: "xpool_allocation").trim()
 	
 	ArrayList arrList = res.split(",")
 	println origin
