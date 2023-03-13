688296483f86e789235f7fc62ba83c5706e54ef6
Dov_Dembin <Dov.Dembin@Dell.com>
2023-03-13 22:55:32 +0200

    dd

--- vars/otel.groovy
+++ vars/otel.groovy
@@ -1,15 +1,30 @@
+@NonCPS
 def getLabels(String text) {
-	// def pattern = /.*\s-l\s(.*)/
-	// if(text ==~ pattern) {
-	// 	def (word1) = text =~ pattern
-	// 	def lblList = word1[1].toString().replaceAll(/\\\|/, ",").trim()
-	// 	ArrayList listlabes = labels.split(",")
-		def cmd = "curl -s --location 'http://labjungle.devops.xiodrm.lab.emc.com/api/v1/cluster/?name=WK-D0046' --header 'Authorization: ApiKey cute:9703aa016d613b2b21bbb0e6833c3078c811a5d1' | jq -r -j '.objects[] | .tags + \",\" + .generation.name'"
-		def res = sh(script: cmd, returnStdout: true, label: "xpool_allocation")
-	// 	ArrayList ljLabels = res.split(",")
-	// 	return listlabes.intersect(ljLabels)
-	// } else {
-	// 	return "noMatch"
-	// }
+	def pattern = /.*\s-l\s(.*)/
+	if(text ==~ pattern) {
+		def (word1) = text =~ pattern
+		def lblList = word1[1].toString().replaceAll(/\\\|/, ",").trim()
+		return getIntersection(lblList)
+	} else {
+		return "noMatch"
+	}
 	 
 }
+
+
+ArrayList<String> getIntersection(String labels){
+	ArrayList listlabes = labels.split(",")
+	// println listlabes
+	def res = sh(script: """
+	curl -s --location 'http://labjungle.devops.xiodrm.lab.emc.com/api/v1/cluster/?name=WK-D0046' \
+	--header 'Authorization: ApiKey cute:9703aa016d613b2b21bbb0e6833c3078c811a5d1' | \
+	jq -r -j '.objects[] | .tags + \",\" + .generation.name'
+	""", returnStdout: true, label: "xpool_allocation")
+	 	
+	ArrayList ljLabels = res.split(",")
+	return listlabes.intersect(ljLabels)
+}
+
+// config_params       = ['xpoolAllocation':'--lg 1 -l indus1\\|indus2\\|indus3,PhysicalLG,powerActionSupported,@Indus_Not_Aligned']
+
+// println getLabels(config_params['xpoolAllocation'])
