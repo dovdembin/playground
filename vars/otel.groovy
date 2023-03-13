000c0ff7c89c7698f5158300191b35e75193e684
Dov_Dembin <Dov.Dembin@Dell.com>
2023-03-13 18:44:01 +0200

    dff

--- vars/otel.groovy
+++ vars/otel.groovy
@@ -1,9 +1,10 @@
 def getLabels(String text) {
-	def pattern = /.*\s-l\s(.*)/
-	if(text ==~ pattern) {
-		def (word1) = text =~ pattern
-		return  word1[1].toString().replaceAll(/\\\|/, ",").trim()
-	} else {
-		return "noMatch"
-	}
+	cmd ="curl -s --location 'http://labjungle.devops.xiodrm.lab.emc.com/api/v1/cluster/?name=WK-D0115' --header 'Authorization: ApiKey cute:9703aa016d613b2b21bbb0e6833c3078c811a5d1' | jq '.objects[].tags'"
+	def res = sh(script: cmd, returnStdout: true, label: "xpool_allocation").trim()
+	println res
 }
+
+
+// config_params       = ['xpoolAllocation':'--lg 1 -l indus1\\|indus2\\|indus3,PhysicalLG,powerActionSupported,@Indus_Not_Aligned']
+
+// println getLabels(config_params['xpoolAllocation'])
