a87141903690730c24c4232426b972a6d793feae
Dov_Dembin <Dov.Dembin@Dell.com>
2023-03-13 20:09:19 +0200

    s

--- vars/otel.groovy
+++ vars/otel.groovy
@@ -1,10 +1,10 @@
 def getLabels(String text) {
-	def textorigin = "MLK-EX1,MLK-EX2,MLK-EX3,MLK-EX4,PhysicalLG"
+	def textorigin = "EX,MLK,PhysicalLG"
 	ArrayList origin = textorigin.split(",")
 	// cmd ="curl -s --location 'http://labjungle.devops.xiodrm.lab.emc.com/api/v1/cluster/?name=WK-D0115' --header 'Authorization: ApiKey cute:9703aa016d613b2b21bbb0e6833c3078c811a5d1' | jq '.objects[].tags'"
 	
 	res = sh(script: """
-	curl -s --location 'http://labjungle.devops.xiodrm.lab.emc.com/api/v1/cluster/?name=WK-D0115' \
+	curl -s --location 'http://labjungle.devops.xiodrm.lab.emc.com/api/v1/cluster/?name=WK-D0046' \
 	--header 'Authorization: ApiKey cute:9703aa016d613b2b21bbb0e6833c3078c811a5d1' | \
 	jq '.objects[].tags'
 	""", returnStdout: true, label: "xpool_allocation").replaceAll(/\"/, "")
