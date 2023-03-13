f84061c923f69ad18a1f2b6c28d93a9ff87cfd7d
Dov_Dembin <Dov.Dembin@Dell.com>
2023-03-13 20:04:54 +0200

    s

--- vars/otel.groovy
+++ vars/otel.groovy
@@ -1,4 +1,6 @@
 def getLabels(String text) {
+	def textorigin = "MLK-EX1\\|MLK-EX2\\|MLK-EX3\\|MLK-EX4,PhysicalLG"
+	def origin = textorigin.split(",")
 	// cmd ="curl -s --location 'http://labjungle.devops.xiodrm.lab.emc.com/api/v1/cluster/?name=WK-D0115' --header 'Authorization: ApiKey cute:9703aa016d613b2b21bbb0e6833c3078c811a5d1' | jq '.objects[].tags'"
 	
 	res = sh(script: """
@@ -8,9 +10,7 @@ def getLabels(String text) {
 	""", returnStdout: true, label: "xpool_allocation").replaceAll(/\"/, "")
 	
 	ArrayList arrList = res.split(",")
-	println(arrList.contains("DPE:NVME:MTC_8GBMN")); 
-	println(arrList.contains("DPE:NVME:MTC_8GBMN, DPE:NVME:PB23F02T"));
-	println(arrList.contains("DPE"));
+	println origin.intersect(arrList)
 }
 
 
