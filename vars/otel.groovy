9ad1bbbd2e310097ef508d0109476043b2f8b06c
Dov_Dembin <Dov.Dembin@Dell.com>
2023-03-13 22:51:02 +0200

    s

--- vars/otel.groovy
+++ vars/otel.groovy
@@ -3,12 +3,9 @@ def getLabels(String text) {
 	if(text ==~ pattern) {
 		def (word1) = text =~ pattern
 		def lblList = word1[1].toString().replaceAll(/\\\|/, ",").trim()
-		ArrayList listlabes = lblList.split(",")
-		def res = sh(script: """
-								curl -s --location 'http://labjungle.devops.xiodrm.lab.emc.com/api/v1/cluster/?name=WK-D0046' \
-								--header 'Authorization: ApiKey cute:9703aa016d613b2b21bbb0e6833c3078c811a5d1' | \
-								jq -r -j '.objects[] | .tags + "," + .generation.name'
-							""", returnStdout: true, label: "xpool_allocation")
+		ArrayList listlabes = labels.split(",")
+		def cmd = "curl -s --location 'http://labjungle.devops.xiodrm.lab.emc.com/api/v1/cluster/?name=WK-D0046' --header 'Authorization: ApiKey cute:9703aa016d613b2b21bbb0e6833c3078c811a5d1' | jq -r -j '.objects[] | .tags + "," + .generation.name'"
+		def res = sh(script: cmd, returnStdout: true, label: "xpool_allocation")
 		ArrayList ljLabels = res.split(",")
 		return listlabes.intersect(ljLabels)
 	} else {
