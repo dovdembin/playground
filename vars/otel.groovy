1aaae1c36dbd11467e745d66e99a619bb7560a09
Dov_Dembin <Dov.Dembin@Dell.com>
2023-03-14 02:10:53 +0200

    d

--- vars/otel.groovy
+++ vars/otel.groovy
@@ -20,24 +20,23 @@ def checkLabels(String text, String appliance) {
 @NonCPS
 def getLabels(String str, String rig) {
 	println rig
-	// def pattern = /.*\s-l\s(.*)/
-	// def lblList
-	// if(text2 ==~ pattern) {
-	// 	def (word1) = text2 =~ pattern
-	// 	lblList = word1[1].toString().replaceAll(/\\\|/, ",").trim()
-	// } else {
-	// 	return "noMatch"
-	// }
-	// def res = sh(script:"""
-	// 						curl -s --location 'http://labjungle.devops.xiodrm.lab.emc.com/api/v1/cluster/?name=${appliance2}' \
-	// 						--header 'Authorization: ApiKey cute:9703aa016d613b2b21bbb0e6833c3078c811a5d1' | \
-	// 						jq -r -j '.objects[] | .tags + \",\" + .generation.name'
-	// 					""", returnStdout: true, label: "xpool_allocation")
+	def pattern = /.*\s-l\s(.*)/
+	def lblList
+	if(text2 ==~ pattern) {
+		def (word1) = text2 =~ pattern
+		lblList = word1[1].toString().replaceAll(/\\\|/, ",").trim()
+	} else {
+		return "noMatch"
+	}
+	def res = sh(script:"""
+							curl -s --location 'http://labjungle.devops.xiodrm.lab.emc.com/api/v1/cluster/?name=${appliance2}' \
+							--header 'Authorization: ApiKey cute:9703aa016d613b2b21bbb0e6833c3078c811a5d1' | \
+							jq -r -j '.objects[] | .tags + \",\" + .generation.name'
+						""", returnStdout: true, label: "xpool_allocation")
 
-	// ArrayList ljLabels = res.split(",")
-	// ArrayList listlabes = lblList.split(",")
-    // return listlabes.intersect(ljLabels).join(", ")
-	return "klj,lkj"
+	ArrayList ljLabels = res.split(",")
+	ArrayList listlabes = lblList.split(",")
+    return listlabes.intersect(ljLabels).join(", ")
 }
 
 @NonCPS
