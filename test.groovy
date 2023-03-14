86e54b29348bd025b71c8d0d8043ca832ed0c919
Dov_Dembin <Dov.Dembin@Dell.com>
2023-03-14 09:31:26 +0200

    ss

--- test.groovy
+++ test.groovy
@@ -87,9 +87,9 @@
                 //         -a test.name=koko 
                 //     """
                 // }
-    def config_params = ['xpoolAllocation':'--lg 1 -l @indus1\\|@indus2\\|@indus3,MLK']
+def config_params = ['xpoolAllocation':'--federation 1,1 -l EX\\|MLK,PhysicalLG']
 
-    println otel.checkLabels(config_params['xpoolAldlocation'], "");
+    println otel.checkLabels(config_params['xpoolAllocation'], "WK-D0077-WK-D0661-TAG");
         }
     // def map = [
     //     endpoint:env.OTEL_EXPORTER_OTLP_ENDPOINT, 
