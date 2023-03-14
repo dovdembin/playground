218b2975485542705138c0fce50df4ecf7445c22
Dov_Dembin <Dov.Dembin@Dell.com>
2023-03-14 02:35:32 +0200

    d

--- vars/otel.groovy
+++ vars/otel.groovy
@@ -1,20 +1,19 @@
 
 def checkLabels(String text, String appliance) {
 	def pattern = /([A-Z][A-Z]-[A-Z]\d\d\d\d)-([A-Z][A-Z]-[A-Z]\d\d\d\d)-TAG/
+	def m1
+	def m2
 	if(appliance ==~ pattern){
-		def (res1) = (appliance =~ pattern)[0]
-		println res1[1]
-		def m1 = res1[1]
-		def m2 = res1[2]
-		 
-		def list1 = getLabels(text, m1)
-		def list2 = getLabels(text, m2)
-		def createdList = list1 + list2
-		return createdList
+		def (res1) = appliance =~ pattern
+		m1 = res1[1]
+		m2 = res1[2]
 	}
 	else {
 		return getLabels(text, appliance)
 	}
+	def list1 = getLabels(text, m1)
+	def list2 = getLabels(text, m2)
+	def createdList = list1 + list2
 }
 
 
@@ -37,12 +36,4 @@ def getLabels(String str, String rig) {
 	ArrayList ljLabels = res.split(",")
 	ArrayList listlabes = lblList.split(",")
     return listlabes.intersect(ljLabels).join(", ")
-}
-
-def getLj(String machine) {
-	def res =  script.sh(script:"""
-							curl -s --location 'http://labjungle.devops.xiodrm.lab.emc.com/api/v1/cluster/?name=${machine}' \
-							--header 'Authorization: ApiKey cute:9703aa016d613b2b21bbb0e6833c3078c811a5d1' | \
-							jq -r -j '.objects[] | .tags + \",\" + .generation.name'
-						""", returnStdout: true, label: "xpool_allocation")
 }
