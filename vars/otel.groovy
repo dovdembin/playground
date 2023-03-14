4a2f644e4ec03318af0699c54bee8355746d3d81
Dov_Dembin <Dov.Dembin@Dell.com>
2023-03-14 02:11:49 +0200

    sd

--- vars/otel.groovy
+++ vars/otel.groovy
@@ -22,14 +22,14 @@ def getLabels(String str, String rig) {
 	println rig
 	def pattern = /.*\s-l\s(.*)/
 	def lblList
-	if(text2 ==~ pattern) {
-		def (word1) = text2 =~ pattern
+	if(str ==~ pattern) {
+		def (word1) = str =~ pattern
 		lblList = word1[1].toString().replaceAll(/\\\|/, ",").trim()
 	} else {
 		return "noMatch"
 	}
 	def res = sh(script:"""
-							curl -s --location 'http://labjungle.devops.xiodrm.lab.emc.com/api/v1/cluster/?name=${appliance2}' \
+							curl -s --location 'http://labjungle.devops.xiodrm.lab.emc.com/api/v1/cluster/?name=${rig}' \
 							--header 'Authorization: ApiKey cute:9703aa016d613b2b21bbb0e6833c3078c811a5d1' | \
 							jq -r -j '.objects[] | .tags + \",\" + .generation.name'
 						""", returnStdout: true, label: "xpool_allocation")
